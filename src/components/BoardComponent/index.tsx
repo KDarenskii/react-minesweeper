43925fa50827196487583c18d4227eb51927046c
Kirill Darenskii <kdarenskii@mail.ru>
2023-02-27 16:54:24 +0300

    Initialize project

---
+++ src/components/BoardComponent/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const BoardComponent: React.FC = () => {
+    return <div>BoardComponent</div>;
+};
+
+export default BoardComponent;
---
+++ src/components/CellComponent/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const CellComponent: React.FC = () => {
+    return <div>CellComponent</div>;
+};
+
+export default CellComponent;
